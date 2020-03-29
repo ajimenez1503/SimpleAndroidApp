2364aa4c18b16cb272928d11eb2703e2e0a0ad3b
Antonio Jimenez <ajimenez@rti.com>
2020-03-29 19:49:50 +0200

    Add button for hour

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.myfirstapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final String EXTRA_HOUR = "com.example.myfirstapp.HOUR";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +24,19 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText = findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    /**
+     * Called when the user taps the Hour button
+     */
+    public void displayHour(View view) {
+        Date currentDate = new Date();
+        Intent intent = new Intent(this, displayHourActivity.class);
+        intent.putExtra(EXTRA_HOUR, currentDate.toString());
         startActivity(intent);
     }
 }
